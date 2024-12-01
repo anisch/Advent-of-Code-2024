f47c7ee9ade8811b5d08a188257f15c3ce9d06fe
anisch <8927916+anisch@users.noreply.github.com>
2024-12-01 10:30:02 +0100

    day 01

---
+++ kotlin/main/Day01.kt
@@ -0,0 +1,46 @@
+import Day01.part1
+import Day01.part2
+import kotlin.math.absoluteValue
+
+fun main() {
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 11)
+    val input = readInput("Day01")
+    println(part1(input))
+
+    check(part2(testInput) == 31)
+    println(part2(input))
+}
+
+private fun List<String>.toIntListPair(): Pair<List<Int>, List<Int>> {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+
+    this.forEach { line ->
+        val split = line.split("\\s+".toRegex())
+        left.add(split.first().toInt())
+        right.add(split.last().toInt())
+    }
+
+    left.sort()
+    right.sort()
+
+    return left to right
+}
+
+object Day01 {
+    fun part1(input: List<String>): Int {
+        val (left, right) = input.toIntListPair()
+
+        return left
+            .mapIndexed { idx, l -> (l - right[idx]).absoluteValue }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        val (left, right) = input.toIntListPair()
+
+        return left
+            .sumOf { l -> l * right.count { r -> r == l } }
+    }
+}
